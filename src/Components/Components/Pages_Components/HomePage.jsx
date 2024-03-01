f45b0a6c520603e8717e67a46229e8c97aee535a
Prajwal <prajwalkotagi06@gmail.com>
2024-03-01 17:34:55 +0530

    Desktop

--- src/Components/Components/Pages_Components/HomePage.jsx
+++ src/Components/Components/Pages_Components/HomePage.jsx
@@ -7,7 +7,7 @@ import Footer from '../Footer/Footer'
 const HomePage = () => {
   return (
     <>
-        <div style={{ margin: '140px',marginBottom: '170px' }}>
+        <div style={{ margin: '140px',marginBottom: '200px' }}>
           <h1 style={{ fontSize: '40px', textTransform: 'uppercase', color: 'whitesmoke' }}>I'M Prajwal Kotagi</h1>
           <p style={{ fontSize: '25px', fontWeight: '500', color: 'whitesmoke' }}>I'm a passionate and results-driven web developer with a proven track record of designing and developing cutting-edge web solutions. With a strong foundation in front-end and back-end development technologies, I specialize in crafting responsive, mobile-friendly websites that meet the unique needs of clients across diverse industries. I excel in turning ideas into reality, working closely with cross-functional teams to ensure that projects are delivered on time.</p>
           <Link to="/About" style={{ fontSize: '20px', color: 'whitesmoke', textDecoration: 'none' }}>See More About Me <ArrowRightOutlined /></Link>
